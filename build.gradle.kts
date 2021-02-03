eef5b479c4b1dd21159e7ceb9d9da577716aedf9
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-02-03 20:27:54 +0900

    Update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,43 +1,36 @@
 plugins {
-    id("org.jetbrains.kotlin.js") version "1.4.0"
+    kotlin("js") version "1.4.21"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    maven("https://kotlin.bintray.com/kotlin-js-wrappers/")
-    mavenCentral()
     jcenter()
+    mavenCentral()
+    maven { url = uri("https://dl.bintray.com/kotlin/kotlin-js-wrappers") }
 }
 
 dependencies {
-    implementation(kotlin("stdlib-js"))
+    implementation("org.jetbrains:kotlin-react:17.0.1-pre.144-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-react-dom:17.0.1-pre.144-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-react-router-dom:5.2.0-pre.144-kotlin-1.4.21")
+    implementation("org.jetbrains:kotlin-styled:5.2.0-pre.144-kotlin-1.4.21")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.2")
 
-    implementation("org.jetbrains:kotlin-react:16.13.1-pre.112-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-react-dom:16.13.1-pre.112-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-react-router-dom:5.1.2-pre.112-kotlin-1.4.0")
-    implementation("org.jetbrains:kotlin-styled:1.0.0-pre.112-kotlin-1.4.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.9")
-
-    implementation(npm("@fluentui/react", "~7.134.1"))
-    implementation(npm("inline-style-prefixer", "~6.0.0"))
-    implementation(npm("react", "16.13.1"))
-    implementation(npm("react-dom", "16.13.1"))
-    implementation(npm("styled-components", "~5.2.0"))
+    implementation(npm("@fluentui/react", "~7.158.0"))
 }
 
 kotlin {
-    js {
+    js(LEGACY) {
         browser {
+            binaries.executable()
             webpackTask {
                 cssSupport.enabled = true
             }
-
             runTask {
                 cssSupport.enabled = true
             }
-
             testTask {
                 useKarma {
                     useChromeHeadless()
@@ -45,6 +38,5 @@ kotlin {
                 }
             }
         }
-        binaries.executable()
     }
 }
