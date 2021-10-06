7d559e74a3cf0b238ba49fcd4a0db8d6c647cbd0
tmyksj <33417830+tmyksj@users.noreply.github.com>
2021-10-06 20:26:12 +0900

    Add d3-hierarchy declarations

---
+++ src/main/kotlin/app/js/d3/hierarchy/hierarchy.kt
@@ -0,0 +1,7 @@
+@file:JsModule("d3-hierarchy")
+@file:JsNonModule
+
+package app.js.d3.hierarchy
+
+@JsName("hierarchy")
+external fun hierarchy(data: dynamic): dynamic
---
+++ src/main/kotlin/app/js/d3/hierarchy/pack.kt
@@ -0,0 +1,7 @@
+@file:JsModule("d3-hierarchy")
+@file:JsNonModule
+
+package app.js.d3.hierarchy
+
+@JsName("pack")
+external fun pack(): dynamic
